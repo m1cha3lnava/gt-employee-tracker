d4344a8de8e18ffbf3bd7953c18ac65be58a42ae
Michael Nava <M1cha3l.nava@gmail.com>
2020-07-08 22:48:01 -0400

    added seeds and package.json

---
+++ app.js
@@ -0,0 +1,24 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Mwd768!!!",
+  database: "ice_creamDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  afterConnection();
+});
+
+function afterConnection() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
+}
